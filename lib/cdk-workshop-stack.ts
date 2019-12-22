f55992497566f755aa1a788dc75aeff8a8066229
navneev <navneev@amazon.com>
2019-12-22 13:46:19 +0530

    Createa a Lambda, VPC and API gatewat with CDK in node.

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,38 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // creating a VPC
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: ec2.SubnetType.ISOLATED,
+        }
+      ]
+    });
+    
+    // create a lambda
+    const handler = new lambda.Function(this, "Lambda", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: new lambda.AssetCode("resources"),
+      handler: "index.hello_world",
+      vpc: vpc,
+      vpcSubnets:
+      {
+        subnetType: ec2.SubnetType.ISOLATED
+      }
+    });
 
-    // The code that defines your stack goes here
+    // create the apigw for lambda
+
+    const apigateway = new apigw.LambdaRestApi(this, "myApigatw", {
+      handler: handler
+    });
   }
 }
